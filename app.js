61031e51509ded45642122afe6723053e3171df3
Tang Jérémy <jeremy.tang@efrei.net>
2022-02-09 10:55:47 +0100

    4

--- app.js
+++ app.js
@@ -4,31 +4,10 @@ const host = 'localhost';
 const port = 8081;
 
 
-const MongoDBStore = require('connect-mongodb-session')(session);
-        const store = new MongoDBStore({
-            uri: MONGODB_URI,
-            collection: 'sessions'
-        });
-        app.use(
-            session({	
-                secret: 'secret string',
-                resave: false,
-                saveUninitialized: false,
-                store: store, /* store session data in mongodb */ 
-                cookie: { /* can add cookie related info here */ }
-            })
-        );
+app.get('/', (req, res) => {
+    res.send(`Hello from my node app, it is your first time here ! Welcome :)`);
 
-
-app.get('/', function(req, res){
-    if(!req.session.pageCountByCurrentUserOrAnyNameYouWant)
-        req.session.pageCountByCurrentUserOrAnyNameYouWant = 0;
-    req.session.pageCountByCurrentUserOrAnyNameYouWant++;
-    res.send({
-        pageCount: req.session.pageCountByCurrentUserOrAnyNameYouWant
-    });
 });
-
 
 app.listen(port, () => {
 });
