9a9c1df03834bbfc6c08c289785cc756ab924286
Tang Jérémy <jeremy.tang@efrei.net>
2022-02-09 11:26:44 +0100

    8

--- app.js
+++ app.js
@@ -4,6 +4,23 @@ const port = 8081;
 const session = require('express-session');
 const app = express();
 
+app.use (
+    session ({
+
+        // It holds the secret key for session
+        secret: "I am girl",
+
+        // Forces the session to be saved
+        // back to the session store
+        resave: true,
+        
+        
+        // Forces a session that is "uninitialized"
+        // to be saved to the store
+        saveUninitialized: false,
+        cookie: {
+    })
+);
 app.get('/', (req, res) => {
     res.send(`Hello from my node app, it is your first time here ! Welcome :)`);
 
