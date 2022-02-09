5ecc0372e3e3e7c47523edecc48cda61d7bb0dd0
Tang Jérémy <jeremy.tang@efrei.net>
2022-02-09 11:08:32 +0100

    5

--- app.js
+++ app.js
@@ -2,12 +2,30 @@ const express = require('express');
 const app = express();
 const host = 'localhost';
 const port = 8081;
-
+const session = require('express-session');
 
 app.get('/', (req, res) => {
     res.send(`Hello from my node app, it is your first time here ! Welcome :)`);
 
 });
+
+// Get function in which send session as routes.
+app.get('/session', function(req, res, next) {
+
+    if (req.session.views) {
+        
+    // Increment the number of views.
+    req.session.views++
+
+    // Print the views.
+    res.write('<p> No. of views: '
+        + req.session.views + '</p>')
+    res.end()
+    } else {
+    req.session.views = 1
+    res.end(' New session is started')
+    }
+})
 
 app.listen(port, () => {
 });
