785b64c96b5dbbb1f3025de2cfe9d028dae225ee
Tartatum <45455096+Tartatum@users.noreply.github.com>
2022-02-11 17:55:35 +0100

    Comm remove
    Remove Commentary

--- app.js
+++ app.js
@@ -4,24 +4,7 @@ const port = 8081;
 const session = require('express-session');
 const app = express();
 
-// Session Setup
-app.use (
-    session ({
 
-        // It holds the secret key for session
-        secret: "I am girl",
-
-        // Forces the session to be saved
-        // back to the session store
-        resave: true,
-        
-        
-        // Forces a session that is "uninitialized"
-        // to be saved to the store
-        saveUninitialized: false,
-        cookie: {}
-    })
-);
 app.get('/', (req, res) => {
     res.send(`Hello from my node app, it is your first time here ! Welcome :)`);
 
@@ -30,10 +13,10 @@ app.get('/session', function(req, res, next) {
 
     if (req.session.views) {
         
-    // Increment the number of views.
+    
     req.session.views++
 
-    // Print the views.
+    
     res.write('<p> No. of views: '
         + req.session.views + '</p>')
     res.end()
